7434f4f83f0892665bb7d084faf801f379129a3c
LachezarDimitrov <lachezar_dimitrov@outlook.com>
2021-08-03 15:14:45 +0300

    Read Cloud Firestore messages

--- index.js
+++ index.js
@@ -101,5 +101,22 @@ async function main() {
     // Return false to avoid redirect
     return false;
   });
+
+  // Create query for messages
+  firebase
+    .firestore()
+    .collection('guestbook')
+    .orderBy('timestamp', 'desc')
+    .onSnapshot(snaps => {
+      // Reset page
+      guestbook.innerHTML = '';
+      // Loop through documents in database
+      snaps.forEach(doc => {
+        // Create an HTML entry for each document and add it to the chat
+        const entry = document.createElement('p');
+        entry.textContent = doc.data().name + ': ' + doc.data().text;
+        guestbook.appendChild(entry);
+      });
+    });
 }
 main();
